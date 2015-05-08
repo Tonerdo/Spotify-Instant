046164e7d4332ad4473d40a3305ef80754af45a0
Developer Without Borders <toni.edward@outlook.com>
2015-05-08 13:45:37 +0100

    Refactored javascript code to use OLP

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 
 	$('#search').on('keyup', search);
 	$('#type').on('change', search);
@@ -6,12 +6,15 @@ $(document).ready(function(){
 	function search() {
 
 		$('.results').text('');
+		$('#loading').show();
 
 		var type = $('#type').val();
 		var q = $('#search').val();
 
-		if(q == '')
+		if(q == '') {
 			$('#no').text('');
+			$('#loading').hide();
+		}
 
 		var uri = globalVars.apiHost + '/search?q=' + q + '&limit=50&type=' + type;
 		$.getJSON(uri, function(result) {
@@ -25,7 +28,55 @@ $(document).ready(function(){
 					+ "<a href='" + items[i].uri + "'><img class='play' src='images/play.png' />&nbsp;Play</a>"
 					+ "</div>");
 			};
+
+			$('#loading').hide();
 		});
 	}
 
-})
+});*/
+
+var spotifyInstant = {
+	searchField: $('#search'),
+	typeField: $('#type'),
+	resultList: $('.results'),
+	numResults: $('#no'),
+	loadingText: $('#loading'),
+	initialize: function() {
+		spotifyInstant.searchField.on('keyup', spotifyInstant.search);
+		spotifyInstant.typeField.on('change', spotifyInstant.search);
+	},
+	search: function() {
+
+		spotifyInstant.resultList.text('');
+		spotifyInstant.loadingText.show();
+
+		var type = spotifyInstant.typeField.val();
+		var q = spotifyInstant.searchField.val();
+
+		if(q == '') {
+			spotifyInstant.numResults.text('');
+			spotifyInstant.loadingText.hide();
+		}
+
+		var uri = globalVars.apiHost + '/search?q=' + q + '&limit=50&type=' + type;
+
+		$.getJSON(uri, function(result) {
+			var items = result[type + 's'].items;
+			spotifyInstant.numResults.text(items.length + ' ' + type + 's found');
+			for (var i = 0; i < items.length; i++) {
+				spotifyInstant.resultList.append("<div class='item'>"
+					+ "<img class='thumb' style='width: 64px; height: 64px;' src='" + items[i].images[2].url + "' />"
+					+ "<span>" + items[i].name + "</span>"
+					+ "<p></p>"
+					+ "<a href='" + items[i].uri + "'><img class='play' src='images/play.png' />&nbsp;Play</a>"
+					+ "</div>");
+			};
+
+			spotifyInstant.loadingText.hide();
+		});
+
+	}
+
+};
+
+spotifyInstant.initialize();
